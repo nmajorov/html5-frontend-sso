9e3af72d677b6d3ea528488f620aaa2ed4ddebd2
Nikolaj Majorov <nikolaj@majorov.biz>
2019-10-30 15:57:00 +0100

    add new keycloak test file

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -z "$EAP_HOME" ]; then
- echo "Need to set EAP_HOME env"
- exit
-fi
-    mvn clean package
-    cp ./target/addressbook-html5.war $EAP_HOME/standalone_oidc/deployments/
